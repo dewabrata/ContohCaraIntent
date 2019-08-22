16e843cd74958fee032aed533a5d3485ed506237
Stevan-Iskandar <stevan.iskandar@gmail.com>
2019-08-22 11:29:00 +0700

    Buatan Stevan

--- app/src/main/java/com/juaracoding/contohcaraintent/StevanActivity.java
+++ app/src/main/java/com/juaracoding/contohcaraintent/StevanActivity.java
@@ -8,6 +8,6 @@ public class StevanActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_stevan);
+    setContentView(R.layout.activity_stevan); //
   }
 }
