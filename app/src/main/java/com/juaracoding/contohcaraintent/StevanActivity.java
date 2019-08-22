adff37116490a003e7090ec1ae349cb664db643a
Stevan-Iskandar <stevan.iskandar@gmail.com>
2019-08-22 11:51:07 +0700

    Nambahin text

--- app/src/main/java/com/juaracoding/contohcaraintent/StevanActivity.java
+++ app/src/main/java/com/juaracoding/contohcaraintent/StevanActivity.java
@@ -2,12 +2,16 @@ package com.juaracoding.contohcaraintent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StevanActivity extends AppCompatActivity {
+  TextView txtStevan;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_stevan);
+
+    txtStevan = (TextView) findViewById(R.id.txtStevan);
   }
 }
