4c94462e5ba820529059ae9dc861f564b86887f0
Dewa <dewabrata@gmail.com>
2019-08-22 10:51:02 +0700

    Menambahkan intent sms

--- app/src/main/java/com/juaracoding/contohcaraintent/Form1.java
+++ app/src/main/java/com/juaracoding/contohcaraintent/Form1.java
@@ -1,6 +1,7 @@
 package com.juaracoding.contohcaraintent;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,20 @@ public class Form1 extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Form1.this,Form2.class);
                 startActivity(intent);
+            }
+        });
+
+
+        ((Button)findViewById(R.id.btnSMS)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setData(Uri.parse("smsto:"));  // This ensures only SMS apps respond
+                intent.putExtra("sms_body", "Hello Dari Juara Coding");
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
             }
         });
     }
