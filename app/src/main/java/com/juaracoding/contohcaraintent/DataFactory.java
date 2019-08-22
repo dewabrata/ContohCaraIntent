0bebb4371431904c870d6d94865d26bd6cabb5b5
Dewa <dewabrata@gmail.com>
2019-08-22 14:48:01 +0700

    SpinnerAdapter

---
+++ app/src/main/java/com/juaracoding/contohcaraintent/DataFactory.java
@@ -0,0 +1,27 @@
+package com.juaracoding.contohcaraintent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataFactory {
+
+
+    public List<String> createDataSimpleList(){
+
+        List<String> list = new ArrayList<String>();
+
+        list.add("Maulana");
+        list.add("Christian");
+        list.add("Aldi");
+        list.add("Jon");
+        list.add("Jun");
+        list.add("Bagus");
+        list.add("Usmoyo");
+        list.add("Stevan");
+
+        return list;
+
+    }
+
+
+}
--- app/src/main/java/com/juaracoding/contohcaraintent/Form1.java
+++ app/src/main/java/com/juaracoding/contohcaraintent/Form1.java
@@ -1,39 +1,38 @@
 package com.juaracoding.contohcaraintent;
 
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class Form1 extends AppCompatActivity {
 
+    Spinner list ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.form_satu);
+        setContentView(R.layout.list_layout);
 
-        ((Button)findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Form1.this,Form2.class);
-                startActivity(intent);
-            }
-        });
+        list = (Spinner)findViewById(R.id.spinner);
 
 
-        ((Button)findViewById(R.id.btnSMS)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.setData(Uri.parse("smsto:"));  // This ensures only SMS apps respond
-                intent.putExtra("sms_body", "Hello Dari Juara Coding");
 
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                }
-            }
-        });
+        ArrayAdapter spinnerArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                new DataFactory().createDataSimpleList());
+
+        list.setAdapter(spinnerArrayAdapter);
+
     }
 }
