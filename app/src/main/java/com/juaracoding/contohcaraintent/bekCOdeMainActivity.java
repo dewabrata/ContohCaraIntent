e17cf21fb0b098df0a699c6d4bab471a289cb03c
jun <bekCOde@gmail.com>
2019-08-22 11:42:27 +0700

    bekCOde_team

--- app/src/main/java/com/juaracoding/contohcaraintent/bekCOdeMainActivity.java
+++ app/src/main/java/com/juaracoding/contohcaraintent/bekCOdeMainActivity.java
@@ -9,5 +9,8 @@ public class bekCOdeMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bek_code_main);
+
+
+        //test
     }
 }
